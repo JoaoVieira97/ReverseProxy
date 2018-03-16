fc5007edfa61a190c6fa33feac0165f57c3b0c76
MQuaresma <miguelquaresma97@gmail.com>
2018-03-16 11:34:52 +0000

    UDPAgents are now able to receive more than one message per session

--- src/UDPAgent.java
+++ src/UDPAgent.java
@@ -13,6 +13,7 @@ class UDPAgent{
     public static void main(String[] args){
         try{
             byte[] buf=new byte[10];
+            String msg=null;
             InetAddress mcGroupIP=InetAddress.getByName("239.8.8.8");
             MulticastSocket receiveSkt=new MulticastSocket(8888); //create multicast socket that listens on port 8888
             DatagramPacket probeRequest=new DatagramPacket(buf, 10);
@@ -20,29 +21,18 @@ class UDPAgent{
             System.out.println("Recieving at: "+mcGroupIP);
             receiveSkt.joinGroup(mcGroupIP);
 
-            System.out.println("Waiting for data...");
-            receiveSkt.receive(probeRequest);
-
-            String msg=new String(probeRequest.getData(),probeRequest.getOffset(),probeRequest.getLength());
-            System.out.println("Recieved: "+msg);
-
+            do{
+                System.out.println("Waiting for data...");
+                receiveSkt.receive(probeRequest);
+                msg=new String(probeRequest.getData(),probeRequest.getOffset(),probeRequest.getLength());
+                System.out.println("Recieved: "+msg);
+            }while(!msg.equals("CT"));
+            
             receiveSkt.leaveGroup(mcGroupIP);
             receiveSkt.close();
         }catch(Exception e){
             e.printStackTrace();
         }
          
-    }
-}
-
-class UDPListener implements Runnable{
-
-    UDPListener(){
-
-    }
-
-    public void run(){
-
-
     }
 }
